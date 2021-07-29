817f9d93a1d1c04ce2b490078931a02d7f28f00c
liujinwei <jwliu0213@163.com>
2021-07-29 21:24:28 +0800

    乐观锁更新

--- src/main/java/com/ecnu/ljw/second_demo/controller/OrderController.java
+++ src/main/java/com/ecnu/ljw/second_demo/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.ecnu.ljw.second_demo.controller;
 
 import com.ecnu.ljw.second_demo.service.OrderService;
+import com.google.common.util.concurrent.RateLimiter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @RestController
 @Slf4j
@@ -15,6 +18,10 @@ public class OrderController {
 
     @Autowired
     private OrderService orderService;
+
+    // Guava令牌桶：每秒放行10个请求
+    //RateLimiter rateLimiter = RateLimiter.create(10);
+    
     /**
      * 下单接口：导致超卖的错误示范
      * @param sid
@@ -31,4 +38,23 @@ public class OrderController {
         }
         return String.valueOf(id);
     }
+
+    /**
+     * 下单接口：乐观锁更新库存 + 令牌桶限流
+     */
+    @RequestMapping(value="/createOptimisticOrder/{sid}", method=RequestMethod.GET)
+    public String createOptimisticOrder(@PathVariable int sid) {
+        //1、阻塞使获取令牌
+        //log.info("等待时间" + rateLimiter.acquire());
+        int id;
+        try{
+            id = orderService.createOptimisticOrder(sid);
+            log.info("购买成功，剩余库存为: [{}]", id);
+        }catch(Exception e){
+            log.error("购买失败：[{}]", e.getMessage());
+            return "购买失败，库存不足";
+        }
+        return String.format("购买成功，剩余库存为：%d", id);
+    }
+    
 }
